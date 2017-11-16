c4970177c7345a124e61ec58741e2bf0fd8fe69a
Tom Honeybone <tomhon@hotmail.com>
2017-11-16 12:06:54 -0700

    setup logging to cosmosdb

--- app.js
+++ app.js
@@ -1,127 +1,105 @@
-var config = require("./config");
+var restify = require('restify');
+var logResponse = require('./logResponse');
 
-//stub this in when online
-var logResponse = require("./logResponse");
+var server = restify.createServer();
+server.use(restify.plugins.queryParser());
 
-// function bibData(color) {
-//     this.bibColor = color,
-//     this.startLane = null,
-//     this.splitPosition = null,
-//     this.finishPostion = null
-// }
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
 
+function logEntryDataStructure() {
 
+    this.raceCodex = 'R9999', //unique identifier for this race
+    this.phaseID = 'Final', //heat#1, quarters#1, semi#1, final
+    this.sport = 'snowboarding',
+    this.event = 'snowboardCross',
+    this.indexed = 'Yes', 
+    this.temp = '0', //ceclius
+    this.precip =  'None', //None, Rain, Light Snow, Heavy Snow
+    this.gender =  null,
+    this.bibRedStartLane=  null,
+    this.bibGreenStartLane=  null,
+    this.bibBlueStartLane=  null,
+    this.bibBlackStartLane=  null,
+    this.bibWhiteStartLane=  null,
+    this.bibYellowStartLane=  null,
 
-// var logEntry =
+    this.bibRedHolePosition=  null,
+    this.bibGreenHolePosition=  null,
+    this.bibBlueHolePosition=  null,
+    this.bibBlackHolePosition=  null,
+    this.bibWhiteHolePosition=  null,
+    this.bibYellowHolePosition=  null,
 
-// {
-//     raceCodex: 'N9999',
-//     heatID: '1',
-//     sport: 'snowboarding',
-//     event: 'snowboardCross',
-//     indexed: 'Yes', 
-//     temp: '0', 
-//     precip: 'None', 
-//     gender: 'Men',
-//     bibRedStartLane: null,
-//     bibRedSplitPosition: null,
-//     bibRedFinishPosition: null,
-//     bibGreenStartLane: null,
-//     bibGreenSplitPosition: null,
-//     bibGreenFinishPosition: null,
-//     bibBlueStartLane: null,
-//     bibBlueSplitPosition: null,
-//     bibBlueFinishPosition: null,
-//     bibBlackStartLane: null,
-//     bibBlackSplitPosition: null,
-//     bibBlackFinishPosition: null,
-//     bibWhiteStartLane: null,
-//     bibWhiteSplitPosition: null,
-//     bibWhiteFinishPosition: null,
-//     bibYellowStartLane: null,
-//     bibYellowSplitPosition: null,
-//     bibYellowFinishPosition: null
+    this.bibRedSplitPosition=  null,
+    this.bibGreenSplitPosition=  null,
+    this.bibBlueSplitPosition=  null,
+    this.bibBlackSplitPosition=  null,
+    this.bibWhiteSplitPosition=  null,
+    this.bibYellowSplitPosition=  null,
 
-// }
-
-
-// logResponse(logEntry);
-
-// var logEntry =
-
-// {
-//     raceCodex: 'N9999',
-//     heatID: '1',
-//     sport: 'snowboarding',
-//     event: 'snowboardCross',
-//     indexed: 'Yes', 
-//     temp: '0', 
-//     precip: 'None', 
-//     gender: 'Men',
-//     bibRedStartLane: 1,
-//     bibRedSplitPosition: 2,
-//     bibRedFinishPosition: 3,
-//     bibGreenStartLane: 2,
-//     bibGreenSplitPosition: 3,
-//     bibGreenFinishPosition: 4,
-//     bibBlueStartLane: 3,
-//     bibBlueSplitPosition: 4,
-//     bibBlueFinishPosition: 5,
-//     bibBlackStartLane: 4,
-//     bibBlackSplitPosition: 5,
-//     bibBlackFinishPosition: 6,
-//     bibWhiteStartLane: 5,
-//     bibWhiteSplitPosition: 6,
-//     bibWhiteFinishPosition: 1,
-//     bibYellowStartLane: 6,
-//     bibYellowSplitPosition: 1,
-//     bibYellowFinishPosition: 2
-
-// }
+    this.bibRedFinishPosition=  null,
+    this.bibGreenFinishPosition=  null,
+    this.bibBlueFinishPosition=  null,
+    this.bibBlackFinishPosition=  null,
+    this.bibWhiteFinishPosition=  null,
+    this.bibYellowFinishPosition=  null
+};
 
 
-// logResponse(logEntry);
+function parseQueryToLogEntry(query) {
+    var tempLogEntry = new logEntryDataStructure();
 
-var logEntry =
+    tempLogEntry.raceCodex = query.raceCodex, //unique identifier for this race
+    tempLogEntry.phaseID = query.phaseID, //heat#1, quarters#1, semi#1, final
+    tempLogEntry.sport = query.sport,
+    tempLogEntry.event = query.event,
+    tempLogEntry.indexed = query.indexed, 
+    tempLogEntry.temp = query.temp, //ceclius
+    tempLogEntry.precip =  query.precip, //None, Rain, Light Snow, Heavy Snow
+    tempLogEntry.gender =  query.gender,
+    tempLogEntry.bibRedStartLane=  query.bibRedStartLane,
+    tempLogEntry.bibGreenStartLane=  query.bibGreenStartLane,
+    tempLogEntry.bibBlueStartLane=  query.bibBlueStartLane,
+    tempLogEntry.bibBlackStartLane=  query.bibBlackStartLane,
+    tempLogEntry.bibWhiteStartLane=  query.bibWhiteStartLane,
+    tempLogEntry.bibYellowStartLane=  query.bibYellowStartLane,
 
-{
-    raceCodex: 'N9999',
-    phaseID: 'Heat2', //heat#1, quarters#1, semi#1, final
-    sport: 'snowboarding',
-    event: 'snowboardCross',
-    indexed: 'Yes', 
-    temp: '0', 
-    precip: 'None', 
-    gender: 'Men',
-    bibRedStartLane: 6,
-    bibGreenStartLane: 5,
-    bibBlueStartLane: 4,
-    bibBlackStartLane: 3,
-    bibWhiteStartLane: 1,
-    bibYellowStartLane: 2,
+    tempLogEntry.bibRedHolePosition=  query.bibRedHolePosition,
+    tempLogEntry.bibGreenHolePosition=  query.bibGreenHolePosition,
+    tempLogEntry.bibBlueHolePosition=  query.bibBlueHolePosition,
+    tempLogEntry.bibBlackHolePosition=  query.bibBlackHolePosition,
+    tempLogEntry.bibWhiteHolePosition=  query.bibWhiteHolePosition,
+    tempLogEntry.bibYellowHolePosition=  query.bibYellowHolePosition,
 
-    bibRedHolePosition: 1,
-    bibGreenHolePosition:2,
-    bibBlueHolePosition: 5,
-    bibBlackHolePosition: 4,
-    bibWhiteHolePosition: 3,
-    bibYellowHolePosition: 6,
+    tempLogEntry.bibRedSplitPosition=  query.bibRedSplitPosition,
+    tempLogEntry.bibGreenSplitPosition=  query.bibGreenSplitPosition,
+    tempLogEntry.bibBlueSplitPosition=  query.bibBlueSplitPosition,
+    tempLogEntry.bibBlackSplitPosition=  query.bibBlackSplitPosition,
+    tempLogEntry.bibWhiteSplitPosition=  query.bibWhiteSplitPosition,
+    tempLogEntry.bibYellowSplitPosition=  query.bibYellowSplitPosition,
 
-    bibRedSplitPosition: 1,
-    bibGreenSplitPosition:4,
-    bibBlueSplitPosition: 3,
-    bibBlackSplitPosition: 4,
-    bibWhiteSplitPosition: 2,
-    bibYellowSplitPosition: 6,
-
-    bibRedFinishPosition: 3,
-    bibGreenFinishPosition: 1,
-    bibBlueFinishPosition: 2,
-    bibBlackFinishPosition: 5,
-    bibWhiteFinishPosition: 6,
-    bibYellowFinishPosition: 4,
-
-}
+    tempLogEntry.bibRedFinishPosition=  query.bibRedFinishPosition,
+    tempLogEntry.bibGreenFinishPosition=  query.bibGreenFinishPosition,
+    tempLogEntry.bibBlueFinishPosition=  query.bibBlueFinishPosition,
+    tempLogEntry.bibBlackFinishPosition=  query.bibBlackFinishPosition,
+    tempLogEntry.bibWhiteFinishPosition=  query.bibWhiteFinishPosition,
+    tempLogEntry.bibYellowFinishPosition=  query.bibYellowFinishPosition
+    return tempLogEntry;
+};
 
 
-logResponse(logEntry);
+
+server.get('/', function (req, res){
+    console.log("Inbound Request:" , req.query);
+    res.header('Access-Control-Allow-Origin', "*");
+    
+    var logEntry = parseQueryToLogEntry(req.query);
+    logResponse (logEntry);
+    statusUpdateResponse = "Entry Logged";
+    console.log('Outbound Response:', req.query + ":" + statusUpdateResponse);
+    res.send(req.query.status + ":" + statusUpdateResponse);
+    // res.send(session.userData);
+
+});
---
+++ dbsetup_app.js
@@ -0,0 +1,127 @@
+var config = require("./config");
+
+//stub this in when online
+var logResponse = require("./logResponse");
+
+// function bibData(color) {
+//     this.bibColor = color,
+//     this.startLane = null,
+//     this.splitPosition = null,
+//     this.finishPostion = null
+// }
+
+
+
+// var logEntry =
+
+// {
+//     raceCodex: 'N9999',
+//     heatID: '1',
+//     sport: 'snowboarding',
+//     event: 'snowboardCross',
+//     indexed: 'Yes', 
+//     temp: '0', 
+//     precip: 'None', 
+//     gender: 'Men',
+//     bibRedStartLane: null,
+//     bibRedSplitPosition: null,
+//     bibRedFinishPosition: null,
+//     bibGreenStartLane: null,
+//     bibGreenSplitPosition: null,
+//     bibGreenFinishPosition: null,
+//     bibBlueStartLane: null,
+//     bibBlueSplitPosition: null,
+//     bibBlueFinishPosition: null,
+//     bibBlackStartLane: null,
+//     bibBlackSplitPosition: null,
+//     bibBlackFinishPosition: null,
+//     bibWhiteStartLane: null,
+//     bibWhiteSplitPosition: null,
+//     bibWhiteFinishPosition: null,
+//     bibYellowStartLane: null,
+//     bibYellowSplitPosition: null,
+//     bibYellowFinishPosition: null
+
+// }
+
+
+// logResponse(logEntry);
+
+// var logEntry =
+
+// {
+//     raceCodex: 'N9999',
+//     heatID: '1',
+//     sport: 'snowboarding',
+//     event: 'snowboardCross',
+//     indexed: 'Yes', 
+//     temp: '0', 
+//     precip: 'None', 
+//     gender: 'Men',
+//     bibRedStartLane: 1,
+//     bibRedSplitPosition: 2,
+//     bibRedFinishPosition: 3,
+//     bibGreenStartLane: 2,
+//     bibGreenSplitPosition: 3,
+//     bibGreenFinishPosition: 4,
+//     bibBlueStartLane: 3,
+//     bibBlueSplitPosition: 4,
+//     bibBlueFinishPosition: 5,
+//     bibBlackStartLane: 4,
+//     bibBlackSplitPosition: 5,
+//     bibBlackFinishPosition: 6,
+//     bibWhiteStartLane: 5,
+//     bibWhiteSplitPosition: 6,
+//     bibWhiteFinishPosition: 1,
+//     bibYellowStartLane: 6,
+//     bibYellowSplitPosition: 1,
+//     bibYellowFinishPosition: 2
+
+// }
+
+
+// logResponse(logEntry);
+
+var logEntry =
+
+{
+    raceCodex: 'hEAT5',
+    phaseID: 'Final', //heat#1, quarters#1, semi#1, final
+    sport: 'snowboarding',
+    event: 'snowboardCross',
+    indexed: 'Yes', 
+    temp: '0', 
+    precip: 'None', 
+    gender: 'Men',
+    bibRedStartLane: 1,
+    bibGreenStartLane: 3,
+    bibBlueStartLane: 6,
+    bibBlackStartLane: 2,
+    bibWhiteStartLane: 4,
+    bibYellowStartLane: 5,
+
+    bibRedHolePosition: 1,
+    bibGreenHolePosition:2,
+    bibBlueHolePosition: 5,
+    bibBlackHolePosition: 4,
+    bibWhiteHolePosition: 3,
+    bibYellowHolePosition: 6,
+
+    bibRedSplitPosition: 1,
+    bibGreenSplitPosition:4,
+    bibBlueSplitPosition: 3,
+    bibBlackSplitPosition: 4,
+    bibWhiteSplitPosition: 2,
+    bibYellowSplitPosition: 6,
+
+    bibRedFinishPosition: 3,
+    bibGreenFinishPosition: 1,
+    bibBlueFinishPosition: 2,
+    bibBlackFinishPosition: 5,
+    bibWhiteFinishPosition: 6,
+    bibYellowFinishPosition: 4,
+
+}
+
+
+logResponse(logEntry);
